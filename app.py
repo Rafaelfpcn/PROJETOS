20517c35516c94432034cbeaf087693f965bb870
Rafaelfpcn <71890677+Rafaelfpcn@users.noreply.github.com>
2026-04-12 22:21:54 -0300

    Update app.py
    Added a survey feature to collect opinions on service quality.

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-print("⚡ === Calculadora de Consumo Elétrico === ⚡")
+excelente = 0
+bom = 0
+ruim = 0
 
-aparelho = input("Digite o nome do aparelho: ")
-potencia = float(input("Digite a potência do aparelho em watts (W): "))
-horasdia = float(input("Digite o tempo médio de uso diário em horas: "))
+for i in range(1, 51):
+    print(f"\nEntrevistado {i}")
 
-consumomensal = (potencia * horasdia * 30) / 1000
-valorkwh = 0.75
-custoestimado = consumomensal * valorkwh
+    nome = input("Digite o nome: ")
+    idade = int(input("Digite a idade: "))
 
-print("\n📊 === Resultado ===")
-print(f"Aparelho: {aparelho}")
-print(f"Consumo estimado: {consumomensal:.2f} kWh/mês")
-print(f" Custo estimado: R$ {custoestimado:.2f} por mês")
+    print("Opinião sobre o atendimento:")
+    print("1 - EXCELENTE")
+    print("2 - BOM")
+    print("3 - RUIM")
+
+    opiniao = int(input("Digite a opção (1, 2 ou 3): "))
+
+    if opiniao == 1:
+        excelente += 1
+    elif opiniao == 2:
+        bom += 1
+    elif opiniao == 3:
+        ruim += 1
+    else:
+        print("Opção inválida!")
+
+# Resultado final
+print("\n--- RESULTADO DA PESQUISA ---")
+print(f"Quantidade de EXCELENTE: {excelente}")
+print(f"Quantidade de RUIM: {ruim}")
